6da5bd11ff91149778f646c4d0ce272b6202b67c
Yaroslav <yarmoshyk96@gmail.com>
2023-02-28 08:58:18 +0200

    replace subscription destination

--- src/main/java/com/websockets/controller/WebSocketController.java
+++ src/main/java/com/websockets/controller/WebSocketController.java
@@ -5,11 +5,8 @@ import com.websockets.model.Message;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
-
-import java.security.Principal;
 
 @Controller
 public class WebSocketController {
@@ -17,13 +14,12 @@ public class WebSocketController {
     @MessageMapping("/news")
     @SendTo("/topic/news")
     public Greeting broadCastNews(@Payload Message message) {
-        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.name()) + "!");
+        return new Greeting("news");
     }
 
     @MessageMapping("/greetings")
-    @SendToUser("/queue/greetings")
-    public String reply(@Payload String message,
-                        Principal user) {
-        return  "Hello " + message;
+    @SendTo("/queue/greetings")
+    public Greeting reply(@Payload Message message) {
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.name()) + "!");
     }
 }
